03e9bae52f3d37de0ae61d7d8adf0f8586f1b30a
Kyle M Hall <kyle@bywatersolutions.com>
2017-11-30 13:53:30 -0500

    Add .travis.yml & scripts for automatic build and release

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if git log -1 --pretty=oneline | grep -v 'Version auto-incremented'
+then
+  if echo $TRAVIS_BRANCH | grep master
+  then
+    echo "Building release"
+    node increment_version.js
+    git commit -a -m 'Version auto-incremented'
+    gulp build
+    gulp release
+    git remote add github https://$GH_TOKEN@github.com/bywatersolutions/koha-plugin-kitchen-sink
+    git fetch --all
+    git push github HEAD:master
+  fi
+else
+  echo "No release needing."
+fi
