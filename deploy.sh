707d93e0e7aeaabb4e4baf2ac19ebc935d73230f
Kyle M Hall <kyle@bywatersolutions.com>
2017-12-01 07:37:43 -0500

    Update to deploy.sh, don't run for non-master branches

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,17 @@
 
 if git log -1 --pretty=oneline | grep -v 'Version auto-incremented'
 then
-  echo "Building release"
-  node increment_version.js
-  git commit -a -m 'Version auto-incremented'
-  gulp build
-  gulp release
-  git remote add github https://$GH_TOKEN@github.com/bywatersolutions/koha-plugin-kitchen-sink
-  get fetch --all
-  git push github HEAD:master
+  if echo $TRAVIS_BRANCH | grep master
+  then
+    echo "Building release"
+    node increment_version.js
+    git commit -a -m 'Version auto-incremented'
+    gulp build
+    gulp release
+    git remote add github https://$GH_TOKEN@github.com/bywatersolutions/koha-plugin-kitchen-sink
+    get fetch --all
+    git push github HEAD:master
+  fi
 else
   echo "No release needing."
 fi
